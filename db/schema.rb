5f3c9ac4180b0b17ae7ebd22fa21e477b2a5a2a0
ashish-ranjan-profile <ashishranjan2202@gmail.com>
2025-08-13 17:17:34 +0530

    updates and fix user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_02_073726) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_13_114658) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -58,6 +58,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_02_073726) do
     t.string "name"
     t.string "phone"
     t.string "image"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["jti"], name: "index_users_on_jti", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
